e7b240aff1702cef390b82c3872d7c4cd64ae383
eric <tremblay_eric@live.ca>
2014-05-13 23:20:30 -0400

    modification license

--- sudoku.java
+++ sudoku.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Hichem et Eric Tremblay.
+ * Copyright 2011 Eric Tremblay.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
